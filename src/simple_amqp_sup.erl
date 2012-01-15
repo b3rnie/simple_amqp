dd89a42326744e45e8d00e9bf6cc5a14ad9c7bd4
Bjorn Jensen-Urstad <bjorn.urstad@klarna.com>
2012-01-15 23:36:34 +0100

    Usual stuff

---
+++ src/simple_amqp_sup.erl
@@ -0,0 +1,28 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% @doc
+%%% @copyright 2011 Bjorn Jensen-Urstad
+%%% @end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%_* Module declaration ===============================================
+-module(simple_amqp_sup).
+-behaviour(supervisor).
+
+%%%_* Exports ==========================================================
+-export([start_link/1, init/1]).
+
+%%%_* Code =============================================================
+start_link(Args) ->
+  supervisor:start_link({local, ?MODULE}, ?MODULE, Args).
+
+init(Args) ->
+  RestartStrategy = {one_for_one, 1, 10},
+  Kids = {simple_amqp_server, {simple_amqp_server, start_link, []},
+          permanent, 5000, worker, [simple_amqp_server]},
+  {ok, {RestartStrategy, Kids}}.
+
+%%%_* Emacs ============================================================
+%%% Local Variables:
+%%% allout-layout: t
+%%% erlang-indent-level: 2
+%%% End:
